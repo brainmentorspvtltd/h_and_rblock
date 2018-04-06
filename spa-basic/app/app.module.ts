2cf024768e2252fb8420825da4d6b19fd98e2467
amitsrivastava4all <amitsrivastava4all@gmail.com>
2018-04-06 11:15:25 +0530

    Code

---
+++ spa-basic/app/app.module.ts
@@ -0,0 +1,29 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ShopComponent } from './shop/shop.component';
+import { AboutComponent } from './about/about.component';
+import { NewsComponent } from './news/news.component';
+import { HomeComponent } from './home/home.component';
+import {Routes,RouterModule} from '@angular/router';
+import {routes} from './routes';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShopComponent,
+    AboutComponent,
+    NewsComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
